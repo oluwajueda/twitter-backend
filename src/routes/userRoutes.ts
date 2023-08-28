2d1ab9dfd573d84c82eab0e5e77a5ffc7e0728c2
oluwajueda <oluwajuedaloade@gmail.com>
2023-08-29 00:40:55 +0100

    creating user in database

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,26 +1,42 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
 
 
 
 //User CRUD
 
 //create user
-router.post("/", (req, res) =>{
-    res.status(501).json({error: "Not Implemented"});
+router.post("/", async(req, res) =>{
+    const {email, name, username} = req.body
+    console.log(email, name, username);
+    const result = await prisma.user.create({
+       data: {
+        email,
+        name,
+        username,
+        bio: "Hello, I'm new on Twitter"
+       }
+    })
+    
+    res.json(result);
 });
 
 
 // List users
-router.get("/", (req, res) => {
-    res.status(501).json({error: "Not Implemented"})
+router.get("/", async(req, res) => {
+    const allUser = await prisma.user.findMany();
+ 
+    res.json(allUser)
 })
 
 // get one user
-router.get("/:id", (req, res) => {
+router.get("/:id", async(req, res) => {
     const {id} = req.params;
-    res.status(501).json({error: `Not Implemented: ${id}`})
+    const user = await prisma.user.findUnique({where: {id: Number(id)}})
+   res.json(user)
 })
 
 //update user
