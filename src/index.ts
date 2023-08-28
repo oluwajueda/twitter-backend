b323b1d19ef6a3758be08f7a9b69b2896db4e678
oluwajueda <oluwajuedaloade@gmail.com>
2023-08-28 23:25:58 +0100

    add tweet routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes";
+import tweetRoutes from "./routes/tweetRoutes";
 
 
 const app =express();
 
 app.use(express.json());
 app.use("/user",userRoutes)
+app.use("/tweet",tweetRoutes)
 
 
 
